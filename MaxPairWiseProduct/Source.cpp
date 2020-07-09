60f45916311d793e5d53cf46e75faab79547caf8
SimoneFarid <68070890+SimoneFarid@users.noreply.github.com>
2020-07-09 21:59:30 +0200

    Update Source.cpp

--- MaxPairWiseProduct/Source.cpp
+++ MaxPairWiseProduct/Source.cpp
@@ -2,6 +2,12 @@
 #include <iostream> 
 #include <algorithm> 
 using namespace std;
+void print(std::vector<int> const& input)
+{
+    for (int i = 0; i < input.size(); i++) {
+        std::cout << input.at(i) << '   ';
+    }
+}
 
 int main()
 {
@@ -10,28 +16,40 @@ int main()
     int n;
 
     // Read the nb of elements in vect
+    cout << "please enter the number of elements in sequence"<<endl;
     cin >> n;
 
     // Read the vector 
+    cout << "please enter the elements of the sequence"<<endl;
     for (int i = 0; i < n; i++)
     {
         int input;
         cin >> input;
         seq.push_back(input);
     }
+    cout << "sequence you entered" << endl;
+    print(seq);
+
 
     // Find the 1st max element 
     double FisrtMax=*max_element(seq.begin(), seq.end());
+    cout <<endl<< "First Maximum Element is" << endl<< FisrtMax;
 
     // remove the found element
     std::vector<int>::iterator PosF = find(seq.begin(), seq.end(), FisrtMax);
     seq.erase(PosF);
 
+    cout <<endl<< "sequence After removing the 1st maximum element" << endl;
+    print(seq);
+
+
     // Find the 2nd max element
     double SecMax = *max_element(seq.begin(), seq.end());
-    
+    cout <<endl<< "Second Maximum Element is" << endl << SecMax;
+
     //multiply the 2 elements
     int total =  (FisrtMax * SecMax);
-    
-    return total;
+    cout <<endl<<"The Product of the 2 elemnts is  "<< total;
+
+    return 0;
 }
